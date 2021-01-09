91e6aae9be5316eaff41f02115559ed832bb4ab2
rempei-okada <renpei.okada@outlook.com>
2021-01-09 23:24:58 +0900

    Fix useDispatch error

--- react-relux/src/useDispatch.ts
+++ react-relux/src/useDispatch.ts
@@ -4,7 +4,7 @@ import { StoreContext } from "./StoreContext";
 export const useDispatch = () => {
     const store = useContext(StoreContext);
     if (store) {
-        return store.dispatch;
+        return store.dispatch.bind(store);
     }
     else {
         throw new Error("Store is not specified");
